c909615746c33cff0e04de9d5a76d968105dc917
Kenedy Gomes <kndgfls5@gmail.com>
2023-06-05 22:23:44 -0300

    Iniciando pagina de listagem dos cursos!

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,55 @@
 import styled from "styled-components";
-import "./styles.css";
 
 const Header = () => {
-  return <h1>pagina Listagem</h1>;
+  return (
+    <ContainerGroup>
+      <Text>Listagem de cursos</Text>
+      <Containerli>
+        <a href="/">Sair</a>
+      </Containerli>
+    </ContainerGroup>
+  );
 };
 
 export default Header;
+
+const Text = styled.h1`
+  width: 100%;
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+const ContainerGroup = styled.section`
+  width: 100%;
+  display: flex;
+  background-color: #4682b4;
+  color: white;
+  padding: 20px;
+`;
+
+const Containerul = styled.ul`
+  display: flex;
+  list-style: none;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+`;
+
+const Containerli = styled.li`
+  display: flex;
+  margin: 0px 20px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+  list-style: none;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  :hover {
+    color: #87ceeb;
+    transition: 0.5s;
+  }
+`;
--- src/Pages/Listagem-cursos/index.js
+++ src/Pages/Listagem-cursos/index.js
@@ -1,13 +1,52 @@
 import styled from "styled-components";
 import Header from "../../Components/Header";
-import "./styles.css";
 
 const Listagem = () => {
   return (
-    <div className="container-header">
-      <Header />
-    </div>
+    <Container>
+      <ContainerGroup className="container-header">
+        <Header />
+      </ContainerGroup>
+      <ContainerCursos>
+        <h1>Cursos Disponiveis</h1>
+        <ul>
+          <li>
+            <a href="/">Curso 1</a>
+          </li>
+          <li>
+            <a href="/">Curso 1</a>
+          </li>
+          <li>
+            <a href="/">Curso 1</a>
+          </li>
+        </ul>
+      </ContainerCursos>
+    </Container>
   );
 };
 
 export default Listagem;
+
+const ContainerCursos = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    font-size: 30px;
+    font-weight: bold;
+    color: #4682b4;
+    margin-bottom: 20px;
+  }
+`;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: white;
+`;
+
+const ContainerGroup = styled.section`
+  width: 100%;
+`;
